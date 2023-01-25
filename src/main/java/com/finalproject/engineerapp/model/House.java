848297cddda2c07558e55e3fa22bbb7471388915
DIR\neringa.petrauskaite <neringa.petrauskaite@accenture.com>
2023-01-25 10:46:09 +0200

    Add tests for House services

--- src/main/java/com/finalproject/engineerapp/model/House.java
+++ src/main/java/com/finalproject/engineerapp/model/House.java
@@ -21,6 +21,11 @@ public class House {
     public House() {
     }
 
+    public House(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public Long getId() {
         return id;
     }
