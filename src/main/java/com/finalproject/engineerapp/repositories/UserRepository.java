d6c282eb1a753fe40126cc6c7f55174735b7ae3c
NeringaP <neringa.pet@gmail.com>
2023-11-07 17:49:35 +0200

    Add findByUsername()

--- src/main/java/com/finalproject/engineerapp/repositories/UserRepository.java
+++ src/main/java/com/finalproject/engineerapp/repositories/UserRepository.java
@@ -1,13 +1,20 @@
 package com.finalproject.engineerapp.repositories;
 
 import com.finalproject.engineerapp.model.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
+
     List<User> findAll();
+
+//    @Query("SELECT u FROM User u WHERE u.username=:name")
+//    User findByUsername(@Param("name") String username);
+
     User findByUsername(String username);
 }
---
+++ src/main/java/com/finalproject/engineerapp/repositories/UserRepositoryImpl.java
@@ -0,0 +1,27 @@
+package com.finalproject.engineerapp.repositories;
+
+import com.finalproject.engineerapp.model.User;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+@Repository
+public class UserRepositoryImpl {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public User findByUsername(String username) {
+        TypedQuery<User> theQuery = entityManager.createQuery("SELECT u FROM User u WHERE u.username=:username", User.class);
+        theQuery.setParameter("username", username);
+        try {
+            return theQuery.getSingleResult();
+        } catch (javax.persistence.NoResultException e) {
+            // Handle the case when no user with the given username is found.
+            return null;
+        }
+    }
+}
