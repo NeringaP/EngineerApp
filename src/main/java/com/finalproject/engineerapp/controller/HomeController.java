7ef3fc57eafeb92ff395e398f35ec53497437cf9
NeringaP <neringa.pet@gmail.com>
2023-11-21 12:03:30 +0200

    Delete model view from home controller

--- src/main/java/com/finalproject/engineerapp/controller/HomeController.java
+++ src/main/java/com/finalproject/engineerapp/controller/HomeController.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class HomeController {
 
     @GetMapping
-    public String displayHomeView(Model model) {
+    public String displayHomeView() {
         return "home";
     }
 }
