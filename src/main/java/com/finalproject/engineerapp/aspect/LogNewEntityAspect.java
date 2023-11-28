e979b6e9644b78e3dcbafc68d8e88f718d47af09
NeringaP <neringa.pet@gmail.com>
2023-11-28 16:40:05 +0200

    Add aspect order

--- src/main/java/com/finalproject/engineerapp/aspect/LogNewEntityAspect.java
+++ src/main/java/com/finalproject/engineerapp/aspect/LogNewEntityAspect.java
@@ -4,10 +4,12 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(1)
 public class LogNewEntityAspect {
 
     @Before("com.finalproject.engineerapp.aspect.AopExpressions.getServiceSaveAndDeleteMethods()")
